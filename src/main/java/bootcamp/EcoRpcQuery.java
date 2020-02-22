99b7f397aa8fc69fb101df0521a8f27d35e49595
ZhangJi <zhangji@hotmail.com>
2020-02-22 12:48:46 +0800

    ok

---
+++ src/main/java/bootcamp/EcoRpcQuery.java
@@ -0,0 +1,11 @@
+package bootcamp;
+
+public class EcoRpcQuery extends EcoRPC{
+
+    static String docNo = "docNo123";
+
+    public static void main(String[] args) {
+
+        new EcoRpcQuery().queryEco (docNo );
+    }
+}
